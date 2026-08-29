304b4741a18e18b60e3c53637b694b70d43ee36f
Olatunde Ajibola <olatundelawrence64@gmail.com>
2026-08-29 20:37:03 +0100

    adding expense

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let expenseForm = document.querySelector(".input-field");
 let expenseName = document.querySelector("#item");
 let expenseAmount = document.querySelector("#amount");
-let expenseList = document.querySelector(".sub-entry");
+let expenseList = document.querySelector(".entries");
 let total = document.querySelector("#total");
 let addExpense = document.querySelector(".add-button");
 
@@ -10,15 +10,16 @@ addExpense.addEventListener("click", function () {
   let amount = expenseAmount.value;
 
  expenseList.innerHTML += `
-    <div class="sub-entry">
-        <p>${name}</p>
+        <div class="sub-entry">
+            <p>${name}</p>
 
-        <div class="sep">
-            <p>$${amount}</p>
-            <button class="delete-btn" aria-label="Delete entry">
-                🗑️
-            </button>
+            <div class="sep">
+                <p>$${amount}</p>
+
+                <button class="delete-btn" aria-label="Delete entry">
+                    🗑️
+                </button>
+            </div>
         </div>
-    </div>
-`;
+    `;
 });
